1072be2a65329a347f6af4458131d5adbf82b4f5
mnibor <marcelorobin@gmail.com>
2023-01-05 12:39:33 -0300

    segunda clase - variables

---
+++ Clase02.py
@@ -0,0 +1,68 @@
+# VARIABLES
+
+my_first_number = 75
+my_second_number = 35
+
+print('PRIMER VARIABLE:', my_first_number)
+print('SEGUNDA VARIABLE:', my_second_number)
+
+total = my_first_number + my_second_number
+print('LA SUMA ES:', total)
+
+# Buenas prácticas para definir variables
+
+# 1º - Lo ideal sería utilizar variables en inglés
+# 2º - No se pueden usar como nombre de variables, palabras reservadas del lenguaje  (Python)
+# 3º - En Python, la identación del código SI IMPORTA
+# 4º - No se pueden colocar números ni símbolos en el comienzo de los nombres de variable
+# 5º - Se pueden usar acentos y la letra "ñ" pero se contradice con la regla #1
+# 6º - Convenciones:
+#                   formato kebab case: my-first-number, my-second-number
+#                   formato camel case: myFirstNumber, mySecondNumber
+#                   formato snake case: my_first_number, my_second_number
+#                   formato train case: My_First_Number, My_Second_Number
+# 7º - Constantes: se definen con el formato snake case y en mayúsculas todas las palabras. Su valor no debe ser modificado en tiempo de ejecución.
+
+MY_VALUE = 6.33
+
+print(MY_VALUE)
+
+myName = 'Marcelo'
+liveIn = 'Buenos Aires'
+
+print('Me llamo ' + myName + ' y vivo en ' + liveIn)
+
+myName = 3
+liveIn = 65
+
+print('Me llamo ' + str(myName) + ' y vivo en ' + str(liveIn))
+
+# Carácter informativo: posición de memoria
+myName = 'Marcelo'
+liveIn = 'Buenos Aires'
+
+print(id(myName))
+print(id(liveIn))
+
+myName = 4
+liveIn = 66
+
+print(id(myName))
+print(id(liveIn))
+
+# Ejercicio propuesto: generar una variable para almacenar un nombre y un apellido. Otra variable para alojar el email y otra variable para alojar el número de teléfono. Mostrar por pantalla cada dato por separado y además quiero que lo muestren como un texto informativo: "Me llamo XXXX mi email es YYYY y mi número de teléfono es ZZZZ"
+
+# Quiero anexar la edad en formato numérico y que se anexe al texto informativo: "Me llamo XXXX mi email es YYYY y mi número de teléfono es ZZZZ. Tengo CCCC años"
+
+name = 'José Pérez'
+email = 'joseperez@gmail.com'
+telephone = '1122223333'
+age = 38
+
+print(name)
+print(email)
+print(telephone)
+print(age)
+print('')
+print('Me llamo ' + name + ' mi email es ' + email + ' y mi número de teléfono es ' + telephone + '. Tengo ' + str(age) + 'años.')
+
